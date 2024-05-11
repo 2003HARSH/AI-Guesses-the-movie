2c1fe3f2628fe93e0c3726598b485a6471e651bd
Harsh Gupta <97147437+2003HARSH@users.noreply.github.com>
2024-05-11 18:47:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 from worker_file import get_qa_chain
 
 GOOGLEPALM_API_KEY=st.secrets['auth_token']
+os.environ['LANGCHAIN_TRACING_V2']='true'
+os.environ['LANGCHAIN_PROJECT']='ai-guesses-the-movie'
+LANGCHAIN_API-KEY=st.secrets['langchain_key']
 
 st.set_page_config(page_icon='🎬',
                    page_title="AI guesses the movie",                  
